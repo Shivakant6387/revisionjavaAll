aec177fbe5735fc4a3a05b10af7ae454b3ad1ee5
Shivakant6387 <shivakant5875@gmail.com>
2023-01-04 15:30:07 +0530

    Added TreeSet with iterator

---
+++ src/main/java/org/example/TreeSet/TreeSetEx001.java
@@ -0,0 +1,19 @@
+package org.example.TreeSet;
+
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class TreeSetEx001 {
+    public static void main(String[] args) {
+        TreeSet<String>tl=new TreeSet<>();
+        tl.add("Ravi");
+        tl.add("Shiva");
+        tl.add("Mac");
+        tl.add("Ajay");
+        Iterator<String>iterator=tl.iterator();
+        while (iterator.hasNext()){
+            String str=iterator.next();
+            System.out.println(str);
+        }
+    }
+}
