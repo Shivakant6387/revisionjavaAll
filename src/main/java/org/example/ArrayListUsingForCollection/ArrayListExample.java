ced3750b01b2ad06152839831a78bedb632ad58e
Shivakant6387 <shivakant5875@gmail.com>
2023-01-04 14:04:05 +0530

    we are using Array List of the collection framework

---
+++ src/main/java/org/example/ArrayListUsingForCollection/ArrayListExample.java
@@ -0,0 +1,14 @@
+package org.example.ArrayListUsingForCollection;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<String>list=new ArrayList<>();
+        list.add("Mac Vogel");
+        list.add("Mato kato");
+        list.add("Mali ");
+        list.add("Marry");
+        System.out.println(list);
+    }
+}
