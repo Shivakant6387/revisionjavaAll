4747cacd47c814d1b3b2684f6b7cbe2c1c615475
Shivakant6387 <shivakant5875@gmail.com>
2023-01-04 14:10:50 +0530

    we are using ArrayList with for each loop

--- src/main/java/org/example/ArrayListUsingForCollection/ArrayListExample.java
+++ src/main/java/org/example/ArrayListUsingForCollection/ArrayListExample.java
@@ -11,10 +11,15 @@ public class ArrayListExample {
         list.add("Mali ");
         list.add("Marry");
         System.out.println(list);
+        //use Iterator
         Iterator iterator=list.iterator();
         while (iterator.hasNext()){
             String str= (String) iterator.next();
             System.out.println(str);
+        }
+        //use for each loop
+        for (String name:list){
+            System.out.println(name);
         }
     }
 }
